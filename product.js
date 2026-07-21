56a2ff6abdc24b7f5df705670d451f53483d71b1
xxlplz <namiya_kk@hotmail.com>
2026-07-22 05:35:43 +0700

    Update product.js

--- product.js
+++ product.js
@@ -2,15 +2,56 @@ const mainImage = document.getElementById("mainImage");
 
 const thumbs = document.querySelectorAll(".thumb");
 
+let current = 0;
+
+
+// คลิกรูปเล็ก
+
 function changeImage(img){
 
     mainImage.src = img.src;
 
-    thumbs.forEach(t=>t.classList.remove("active"));
+    thumbs.forEach(t => t.classList.remove("active"));
 
     img.classList.add("active");
 
+    current = [...thumbs].indexOf(img);
+
 }
+
+
+// ปุ่มซ้าย
+
+document.querySelector(".left").onclick = function(){
+
+    current--;
+
+    if(current < 0){
+
+        current = thumbs.length - 1;
+
+    }
+
+    changeImage(thumbs[current]);
+
+};
+
+
+// ปุ่มขวา
+
+document.querySelector(".right").onclick = function(){
+
+    current++;
+
+    if(current >= thumbs.length){
+
+        current = 0;
+
+    }
+
+    changeImage(thumbs[current]);
+
+};
 
 function openColor(src){
 
