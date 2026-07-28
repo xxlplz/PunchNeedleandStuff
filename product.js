e447316939892ab0f293551a6470e15f09d4fcdd
xxlplz <namiya_kk@hotmail.com>
2026-07-28 16:20:15 +0700

    Update product.js

--- product.js
+++ product.js
@@ -78,30 +78,3 @@ document.addEventListener("keydown", function(e){
 
 });
 
-document.querySelectorAll(".slider").forEach(slider=>{
-
-    const slides = slider.querySelectorAll(".slide");
-
-    let index = 0;
-
-    slider.querySelector(".next").onclick = ()=>{
-
-        slides[index].classList.remove("active");
-
-        index = (index + 1) % slides.length;
-
-        slides[index].classList.add("active");
-
-    }
-
-    slider.querySelector(".prev").onclick = ()=>{
-
-        slides[index].classList.remove("active");
-
-        index = (index - 1 + slides.length) % slides.length;
-
-        slides[index].classList.add("active");
-
-    }
-
-});
