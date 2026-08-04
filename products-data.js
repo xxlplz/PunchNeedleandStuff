751c16ac97c508af14f1b14050c638da25333d66
xxlplz <namiya_kk@hotmail.com>
2026-08-04 17:30:52 +0700

    Create products-data.js

---
+++ products-data.js
@@ -0,0 +1,24 @@
+const products = [
+
+    {
+        name: "Salmon Sushi Punch Needle Keychain",
+        category: "Keychain",
+        image: "images/punch-needle/keychain-salmon-01.jpg",
+        url: "salmon-sushi.html"
+    },
+
+    {
+        name: "Poison Apple Punch Needle Keychain",
+        category: "Keychain",
+        image: "images/punch-needle/poison-apple-01.jpg",
+        url: "poison-apple.html"
+    },
+
+    {
+        name: "Tomato Coaster",
+        category: "Coaster",
+        image: "images/punch-needle/tomato-coaster-01.jpg",
+        url: "tomato-coaster.html"
+    }
+
+];
