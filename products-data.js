e67f83ec5dc9b6e04f2ea6e5c7c7d6cb790c4c01
xxlplz <namiya_kk@hotmail.com>
2026-08-05 15:31:40 +0700

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -3,7 +3,7 @@ const products = [
     {
         name: "Salmon Sushi Punch Needle Keychain",
         category: "Keychain",
-        keywords: ["salmon","sushi","food","japanese","fish","keychain"],
+        keywords: "salmon sushi food japanese fish keychain punch needle พวงกุญแจ ไหมพรม ปลา แซลมอน ซูชิ อาหาร ญี่ปุ่น ส้ม ปลาส้ม สีส้ม",
         image: "images/punch-needle/keychain-salmon-01.jpg",
         url: "salmon-sushi.html"
     },
@@ -11,7 +11,7 @@ const products = [
     {
         name: "Poison Apple Punch Needle Keychain",
         category: "Keychain",
-        keywords: ["poison apple","halloween","food","snow white","apple","keychain"],
+        keywords: "poison apple halloween food snow white apple keychain punch needle พวงกุญแจ ไหมพรม แอปเปิล ผลไม้ ยาพิษ ฮาโลวีน นิทาน เจ้าหญิง ดิสนีย์ สโนว์ไวท์ red green แดง เขียว สีแดง สีเขียว",
         image: "images/punch-needle/keychain-poison_apple-01.jpg",
         url: "poison-apple.html"
     },
@@ -20,7 +20,7 @@ const products = [
         name: "Tomato Coaster",
         category: "Coaster",
         image: "images/punch-needle/coaster-tomato-out-01.jpg",
-        keywords: ["tomato","coaster","fruit","red","rug"],        
+        keywords: "tomato coaster fruit red rug ที่รองแก้ว ไหมพรม punch needle มะเขือเทศ ผลไม้ ผัก vegetable แดง สีแดง",        
         url: "tomato-fruit-coaster.html"
     }
 
