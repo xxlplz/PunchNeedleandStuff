7b3949395f8a91463a3cd848b6153c29fcd039d5
xxlplz <namiya_kk@hotmail.com>
2026-08-06 21:29:48 +0700

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -1,19 +1,67 @@
 const products = [
 
     {
-        name: "Salmon Sushi Punch Needle Keychain",
+        name: "The Pizza Punch Needle Keychain",
         category: "Keychain",
-        keywords: "salmon sushi food japanese fish keychain punch needle พวงกุญแจ ไหมพรม ปลา แซลมอน ซูชิ อาหาร ญี่ปุ่น ส้ม ปลาส้ม สีส้ม",
-        image: "images/punch-needle/keychain-salmon-01.jpg",
-        url: "salmon-sushi.html"
+        keywords: "Pizza Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร พิซซ่า สีเหลือง สีแดง สีน้ำตาล เหลือง แดง น้ำตาล cheese ชีส red yellow brown",
+        image: "images/punch-needle/keychain-pizza-01.jpg",
+        url: "the-pizza.html"
     },
-
+    
+    {
+        name: "I Love Bacon Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "Bacon Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร หมู เบคอน สีแดง แดง สีขาว ขาว สีชมพู ชมพู pork pink red white",
+        image: "images/punch-needle/keychain-bacon-01.jpg",
+        url: "I-love-bacon.html"
+    },
+    
+    {
+        name: "Hot Dog Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "Hot Dog Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร ฮอตด็อก ไส้กรอก ขนมปัง สีแดง แดง สีน้ำตาล น้ำตาล street สตรีทฟู้ด ซอส สีเหลือง เหลือง bread yellow red brown sausage",
+        image: "images/punch-needle/keychain-bacon-01.jpg",
+        url: "I-love-bacon.html"
+    },
+    
+    {
+        name: "Fried Chicken Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "Fried Chicken Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร ไก่ทอด ไก่ น่องไก่ สีน้ำตาล น้ำตาล กระดูก ขาว สีขาว ของทอด ทอด brown white bone chicken",
+        image: "images/punch-needle/keychain-fried_chicken-01.jpg",
+        url: "fried-chicken.html"
+    },
+    
     {
         name: "Poison Apple Punch Needle Keychain",
         category: "Keychain",
         keywords: "poison apple halloween food snow white apple keychain punch needle พวงกุญแจ ไหมพรม แอปเปิล ผลไม้ ยาพิษ ฮาโลวีน นิทาน เจ้าหญิง ดิสนีย์ สโนว์ไวท์ red green แดง เขียว สีแดง สีเขียว",
         image: "images/punch-needle/keychain-poison_apple-01.jpg",
         url: "poison-apple.html"
+    },
+    
+    {
+        name: "Onigiri Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "Onigiri food japanese keychain punch needle พวงกุญแจ ไหมพรม ข้าวปั้น ข้าวปั้นสามเหลี่ยม สาหร่าย สามเหลี่ยม สีขาว ขาว สีเขียว เขียว white dark green rice",
+        image: "images/punch-needle/kkeychain-onigiri-01.jpg",
+        url: "onigiri.html"
+    },
+
+    {
+        name: "Ume Onigiri Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "Ume Onigiri food japanese keychain punch needle พวงกุญแจ ไหมพรม ข้าวปั้น ข้าวปั้นสามเหลี่ยม บ๋วย สามเหลี่ยม สีขาว ขาว สีแดง แดง white red rice",
+        image: "images/punch-needle/keychain-ume-01.jpg",
+        url: "ume-onigiri.html"
+    },
+    
+    {
+        name: "Salmon Sushi Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "salmon sushi food japanese fish keychain punch needle พวงกุญแจ ไหมพรม ปลา แซลมอน ซูชิ อาหาร ญี่ปุ่น ส้ม ปลาส้ม สีส้ม",
+        image: "images/punch-needle/keychain-salmon-01.jpg",
+        url: "salmon-sushi.html"
     },
 
     {
