cc3610b062679c1dcd73a82c7ea3859661814f3b
xxlplz <namiya_kk@hotmail.com>
2026-08-07 19:02:29 +0700

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -1,71 +1,71 @@
 const products = [
 
     {
-        name: "The Pizza Punch Needle Keychain",
-        category: "Keychain",
+        name: "The Pizza",
+        category: "Punch Needle Keychain Keychain",
         keywords: "Pizza Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร พิซซ่า สีเหลือง สีแดง สีน้ำตาล เหลือง แดง น้ำตาล cheese ชีส red yellow brown",
         image: "images/punch-needle/keychain-pizza-01.jpg",
         url: "the-pizza.html"
     },
     
     {
-        name: "I Love Bacon Punch Needle Keychain",
-        category: "Keychain",
+        name: "I Love Bacon",
+        category: "Punch Needle Keychain Keychain",
         keywords: "Bacon Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร หมู เบคอน สีแดง แดง สีขาว ขาว สีชมพู ชมพู pork pink red white",
         image: "images/punch-needle/keychain-bacon-01.jpg",
         url: "I-love-bacon.html"
     },
     
     {
-        name: "Hot Dog Punch Needle Keychain",
-        category: "Keychain",
+        name: "Hot Dog",
+        category: "Punch Needle Keychain Keychain",
         keywords: "Hot Dog Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร ฮอตด็อก ไส้กรอก ขนมปัง สีแดง แดง สีน้ำตาล น้ำตาล street สตรีทฟู้ด ซอส สีเหลือง เหลือง bread yellow red brown sausage",
         image: "images/punch-needle/keychain-bacon-01.jpg",
         url: "I-love-bacon.html"
     },
     
     {
-        name: "Fried Chicken Punch Needle Keychain",
-        category: "Keychain",
+        name: "Fried Chicken",
+        category: "Punch Needle Keychain Keychain",
         keywords: "Fried Chicken Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร ไก่ทอด ไก่ น่องไก่ สีน้ำตาล น้ำตาล กระดูก ขาว สีขาว ของทอด ทอด brown white bone chicken",
         image: "images/punch-needle/keychain-fried_chicken-01.jpg",
         url: "fried-chicken.html"
     },
     
     {
-        name: "Poison Apple Punch Needle Keychain",
-        category: "Keychain",
+        name: "Poison Apple",
+        category: "Punch Needle Keychain Keychain",
         keywords: "poison apple halloween food snow white apple keychain punch needle พวงกุญแจ ไหมพรม แอปเปิล ผลไม้ ยาพิษ ฮาโลวีน นิทาน เจ้าหญิง ดิสนีย์ สโนว์ไวท์ red green แดง เขียว สีแดง สีเขียว",
         image: "images/punch-needle/keychain-poison_apple-01.jpg",
         url: "poison-apple.html"
     },
     
     {
-        name: "Onigiri Punch Needle Keychain",
-        category: "Keychain",
+        name: "Onigiri",
+        category: "Punch Needle Keychain Keychain",
         keywords: "Onigiri food japanese keychain punch needle พวงกุญแจ ไหมพรม ข้าวปั้น ข้าวปั้นสามเหลี่ยม สาหร่าย สามเหลี่ยม สีขาว ขาว สีเขียว เขียว white dark green rice",
         image: "images/punch-needle/keychain-onigiri-01.jpg",
         url: "onigiri.html"
     },
 
     {
-        name: "Ume Onigiri Punch Needle Keychain",
-        category: "Keychain",
+        name: "Ume Onigiri",
+        category: "Punch Needle Keychain Keychain",
         keywords: "Ume Onigiri food japanese keychain punch needle พวงกุญแจ ไหมพรม ข้าวปั้น ข้าวปั้นสามเหลี่ยม บ๋วย สามเหลี่ยม สีขาว ขาว สีแดง แดง white red rice",
         image: "images/punch-needle/keychain-ume-01.jpg",
         url: "ume-onigiri.html"
     },
     
     {
-        name: "Salmon Sushi Punch Needle Keychain",
-        category: "Keychain",
+        name: "Salmon Sushi",
+        category: "Punch Needle Keychain Keychain",
         keywords: "salmon sushi food japanese fish keychain punch needle พวงกุญแจ ไหมพรม ปลา แซลมอน ซูชิ อาหาร ญี่ปุ่น ส้ม ปลาส้ม สีส้ม",
         image: "images/punch-needle/keychain-salmon-01.jpg",
         url: "salmon-sushi.html"
     },
 
     {
-        name: "Tomato Coaster",
+        name: "Tomato",
         category: "Coaster",
         image: "images/punch-needle/coaster-tomato-out-01.jpg",
         keywords: "tomato coaster fruit red rug ที่รองแก้ว ไหมพรม punch needle มะเขือเทศ ผลไม้ ผัก vegetable แดง สีแดง",        
