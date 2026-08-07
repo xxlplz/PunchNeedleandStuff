06ad17bc1e0bb3a3283e7fae53f89cfddc57a3ad
xxlplz <namiya_kk@hotmail.com>
2026-08-07 19:13:36 +0700

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -3,7 +3,7 @@ const products = [
     {
         name: "The Pizza",
         category: "Punch Needle Keychain Keychain",
-        keywords: "Pizza Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร พิซซ่า สีเหลือง สีแดง สีน้ำตาล เหลือง แดง น้ำตาล cheese ชีส red yellow brown",
+        keywords: "pizza fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร พิซซ่า สีเหลือง สีแดง สีน้ำตาล เหลือง แดง น้ำตาล cheese ชีส red yellow brown",
         image: "images/punch-needle/keychain-pizza-01.jpg",
         url: "the-pizza.html"
     },
@@ -11,7 +11,7 @@ const products = [
     {
         name: "I Love Bacon",
         category: "Punch Needle Keychain Keychain",
-        keywords: "Bacon Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร หมู เบคอน สีแดง แดง สีขาว ขาว สีชมพู ชมพู pork pink red white",
+        keywords: "bacon fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร หมู เบคอน สีแดง แดง สีขาว ขาว สีชมพู ชมพู pork pink red white",
         image: "images/punch-needle/keychain-bacon-01.jpg",
         url: "I-love-bacon.html"
     },
@@ -19,7 +19,7 @@ const products = [
     {
         name: "Hot Dog",
         category: "Punch Needle Keychain Keychain",
-        keywords: "Hot Dog Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร ฮอตด็อก ไส้กรอก ขนมปัง สีแดง แดง สีน้ำตาล น้ำตาล street สตรีทฟู้ด ซอส สีเหลือง เหลือง bread yellow red brown sausage",
+        keywords: "hot dog fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร ฮอตด็อก ไส้กรอก ขนมปัง สีแดง แดง สีน้ำตาล น้ำตาล street สตรีทฟู้ด ซอส สีเหลือง เหลือง bread yellow red brown sausage",
         image: "images/punch-needle/keychain-bacon-01.jpg",
         url: "I-love-bacon.html"
     },
@@ -27,7 +27,7 @@ const products = [
     {
         name: "Fried Chicken",
         category: "Punch Needle Keychain Keychain",
-        keywords: "Fried Chicken Fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร ไก่ทอด ไก่ น่องไก่ สีน้ำตาล น้ำตาล กระดูก ขาว สีขาว ของทอด ทอด brown white bone chicken",
+        keywords: "fried chicken fast food keychain punch needle พวงกุญแจ ไหมพรม อาหาร ไก่ทอด ไก่ น่องไก่ สีน้ำตาล น้ำตาล กระดูก ขาว สีขาว ของทอด ทอด brown white bone chicken",
         image: "images/punch-needle/keychain-fried_chicken-01.jpg",
         url: "fried-chicken.html"
     },
@@ -43,7 +43,7 @@ const products = [
     {
         name: "Onigiri",
         category: "Punch Needle Keychain Keychain",
-        keywords: "Onigiri food japanese keychain punch needle พวงกุญแจ ไหมพรม ข้าวปั้น ข้าวปั้นสามเหลี่ยม สาหร่าย สามเหลี่ยม สีขาว ขาว สีเขียว เขียว white dark green rice",
+        keywords: "onigiri food japanese keychain punch needle พวงกุญแจ ไหมพรม ข้าวปั้น ข้าวปั้นสามเหลี่ยม สาหร่าย สามเหลี่ยม สีขาว ขาว สีเขียว เขียว white dark green rice",
         image: "images/punch-needle/keychain-onigiri-01.jpg",
         url: "onigiri.html"
     },
@@ -51,7 +51,7 @@ const products = [
     {
         name: "Ume Onigiri",
         category: "Punch Needle Keychain Keychain",
-        keywords: "Ume Onigiri food japanese keychain punch needle พวงกุญแจ ไหมพรม ข้าวปั้น ข้าวปั้นสามเหลี่ยม บ๋วย สามเหลี่ยม สีขาว ขาว สีแดง แดง white red rice",
+        keywords: "ume onigiri food japanese keychain punch needle พวงกุญแจ ไหมพรม ข้าวปั้น ข้าวปั้นสามเหลี่ยม บ๋วย สามเหลี่ยม สีขาว ขาว สีแดง แดง white red rice",
         image: "images/punch-needle/keychain-ume-01.jpg",
         url: "ume-onigiri.html"
     },
@@ -59,17 +59,137 @@ const products = [
     {
         name: "Salmon Sushi",
         category: "Punch Needle Keychain Keychain",
-        keywords: "salmon sushi food japanese fish keychain punch needle พวงกุญแจ ไหมพรม ปลา แซลมอน ซูชิ อาหาร ญี่ปุ่น ส้ม ปลาส้ม สีส้ม",
+        keywords: "salmon sushi food japanese fish keychain punch needle พวงกุญแจ ไหมพรม ปลา แซลมอน ซูชิ อาหาร ญี่ปุ่น ส้ม ปลาส้ม สีส้ม ข้าว rice",
         image: "images/punch-needle/keychain-salmon-01.jpg",
         url: "salmon-sushi.html"
     },
 
     {
-        name: "Tomato",
+        name: "Tuna Sushi Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "tuna sushi food japanese fish keychain punch needle พวงกุญแจ ไหมพรม ปลา ทูน่า ซูชิ อาหาร ญี่ปุ่น แดง สีแดง ข้าว rice maguro akami",
+        image: "images/punch-needle/keychain-tuna-01.jpg",
+        url: "tuna-sushi.html"
+    },
+
+    {
+        name: "Ikura Sushi Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "ikura sushi food japanese fish roe keychain punch needle พวงกุญแจ ไหมพรม ไข่ปลา แซลมอน ซูชิ อาหาร ญี่ปุ่น ข้าว rice ห่อสาหร่าย สาหร่าย เขียว สีเขียว deep green orange ส้ม สีส้ม",
+        image: "images/punch-needle/keychain-ikura-01.jpg",
+        url: "ikura-sushi.html"
+    },
+
+    {
+        name: "Tamago Sushi Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "tamago sushi food japanese keychain punch needle พวงกุญแจ ไหมพรม ไข่ ไข่หวาน sweet eggs ข้าว rice เหลือง สีเหลือง yellow ขาว สีขาว white สาหร่าย สีเขียว เขียว dark green",
+        image: "images/punch-needle/keychain-tamago-01.jpg",
+        url: "tamago-sushi.html"
+    },
+
+    {
+        name: "Amaebi Sushi Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "amaebi sushi food japanese keychain punch needle พวงกุญแจ ไหมพรม sweet shrimp pink prawn กุ้ง กุ้งหวาน ชมพู สีชมพู ข้าว rice ขาว สีขาว white",
+        image: "images/punch-needle/keychain-amaebi-01.jpg",
+        url: "amaebi-sushi.html"
+    },
+
+    {
+        name: "Ebi Sushi Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "ebi sushi food japanese keychain punch needle พวงกุญแจ ไหมพรม shrimp prawn กุ้ง ส้ม สีส้ม ขาว สีขาว white orange ข้าว rice",
+        image: "images/punch-needle/keychain-ebi-01.jpg",
+        url: "ebi-sushi.html"
+    },
+
+    {
+        name: "Maki Sushi Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "maki sushi food japanese keychain punch needle พวงกุญแจ ไหมพรม สาหร่าย สีเขียว เขียว green dark white ขาว สีขาว เหลือง สีเหลือง yellow ไข่ แครอท ส้ม สีส้ม orange egg carrot spam สแปม ชมพู pink สีชมพู ข้าว rice",
+        image: "images/punch-needle/keychain-maki-01.jpg",
+        url: "maki-sushi.html"
+    },
+
+    {
+        name: "Dango Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "dango food japanese keychain punch needle พวงกุญแจ ไหมพรม ดังโงะ sweet rice cake ขนม ของหวาน dessert เขียว ชมพู ขาว green pink white สีเขียว สีชมพู สีขาว",
+        image: "images/punch-needle/keychain-dango-01.jpg",
+        url: "dango.html"
+    },
+
+    {
+        name: "Tempura Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "tempura food japanese keychain punch needle พวงกุญแจ ไหมพรม เทมปุระ กุ้ง กุ้งทอด กุ้งชุบแป้งทอด shrimp prawn deep fried เหลือง สีเหลือง yellow ส้ม สีส้ม orange",
+        image: "images/punch-needle/keychain-tempura-01.jpg",
+        url: "tempura.html"
+    },
+
+    {
+        name: "Ghost Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "ghost ผี ผีน้อย keychain punch needle พวงกุญแจ ไหมพรม ขาว สีขาว white ดำ สีดำ black halloween ฮาโลวีน",
+        image: "images/punch-needle/keychain-ghost-01.jpg",
+        url: "ghost.html"
+    },
+
+    {
+        name: "Skull Punch Needle Keychain",
+        category: "Keychain",
+        keywords: "skull ผี กะโหลก หัวกะโหลก keychain punch needle พวงกุญแจ ไหมพรม ขาว สีขาว white ดำ สีดำ black halloween ฮาโลวีน",
+        image: "images/punch-needle/keychain-skull-01.jpg",
+        url: "skull.html"
+    },
+
+    {
+        name: "Tomato Punch Needle Coaster",
         category: "Coaster",
         image: "images/punch-needle/coaster-tomato-out-01.jpg",
         keywords: "tomato coaster fruit red rug ที่รองแก้ว ไหมพรม punch needle มะเขือเทศ ผลไม้ ผัก vegetable แดง สีแดง",        
         url: "tomato-fruit-coaster.html"
+    },
+
+    {
+        name: "Tomato (halves) Punch Needle Coaster",
+        category: "Coaster",
+        image: "images/punch-needle/coaster-tomato-in-01.jpg",
+        keywords: "tomato halves coaster fruit red rug ที่รองแก้ว ไหมพรม punch needle มะเขือเทศ ผลไม้ ผัก vegetable pink ชมพู สีชมพู แดง สีแดง",        
+        url: "tomato-halves-coaster.html"
+    },
+
+    {
+        name: "The Hamburger set Punch Needle Coaster",
+        category: "Coaster",
+        image: "images/punch-needle/coaster-hamburger-set-01.jpg",
+        keywords: "hamburger set coaster fast food rug ที่รองแก้ว ไหมพรม punch needle เซ็ต ขนมปัง bread bun ผัก vegetable เนื้อ meat beef patty brown สีน้ำตาล น้ำตาล เขียว สีเขียว green white ขาว สีขาว",        
+        url: "the-hamburger-set.html"
+    },
+
+    {
+        name: "Floppy Disk Punch Needle Coaster",
+        category: "Coaster",
+        image: "images/punch-needle/coaster-floppy-disk-02.jpg",
+        keywords: "floppy disk coaster rug ที่รองแก้ว ไหมพรม punch needle windows 2000 แผ่นดิส save เซฟ ดำ สีดำ black ส้ม สีส้ม orange เทา สีเทา gray white ขาว สีขาว ไอคอน icon",        
+        url: "floppy-disk.html"
+    },
+
+    {
+        name: "Folder Punch Needle Coaster",
+        category: "Coaster",
+        image: "images/punch-needle/coaster-folder-02.jpg",
+        keywords: "folder coaster rug ที่รองแก้ว ไหมพรม punch needle windows 2000 โฟลเดอร์ สีเหลือง เหลือง yellow ไอคอน icon",        
+        url: "folder.html"
+    },
+
+    {
+        name: "Custom Order Punch Needle",
+        category: "Custom",
+        image: "images/punch-needle/custom-chihuahua-01.jpg",
+        keywords: "custom order keychain พวงกุญแจ ไหมพรม punch needle สั่งทำ ตามสั่ง หมา สุนัข dog pet สัตว์เลี้ยง แมว cat",        
+        url: "custom-orders.html"
     }
 
 ];
